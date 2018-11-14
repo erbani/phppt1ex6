569d2ae2214b10947f7aeffce2ca84bf28c692d1
ERBANI <alexandre.erbani@live.fr>
2018-11-14 10:00:21 +0100

    exercice6

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>PHP Exercice 5 Partie 1</title>
+    </head>
+    <body>
+        <?php
+        $name = 'Robert';
+        ?> 
+        <p>Bonjour <?= $name ?> comment vas-tu?</p>
+    </body>
+</html>
